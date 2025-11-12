9a55758a3ee83e6ecd770962dd9e883c7e93f8fe
fernandaUTZMG <fernanda.hernandez.22s@utzmg.edu.mx>
2025-11-11 21:35:26 -0600

    Backend

--- server.js
+++ server.js
@@ -30,10 +30,11 @@ app.use(cors({
 app.use(express.json());
 
 // -------------------- MongoDB --------------------
-mongoose.connect("mongodb://127.0.0.1:27017/celeste_dos")
+console.log("Mongo URI que usa el backend:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB conectado"))
   .catch(err => console.error("❌ Error Mongo:", err));
-
 // -------------------- AUTH --------------------
 app.post("/register", async (req, res) => {
   try {
